96e348a47f62d3b813fd6979018af898fd5a80da
tuan2linh <champihycoql@gmail.com>
2024-12-04 21:28:22 +0700

    deploy

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.post('/send-email', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
